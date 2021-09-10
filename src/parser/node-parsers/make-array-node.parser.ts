0e01794a855e0af11951f4cdac68cb445ae76c08
Martin Hodler <martin.hodler@gmail.com>
2021-09-10 11:06:30 +0200

    Add make-array icon

--- src/parser/node-parsers/make-array-node.parser.ts
+++ src/parser/node-parsers/make-array-node.parser.ts
@@ -3,6 +3,7 @@ import { NodeParser } from "../node.parser";
 import { ParsingNodeData } from "../parsing-node-data";
 import { HeadedNodeControl } from "../../controls/nodes/headed-node-control";
 import { Constants } from "../../constants";
+import { IconLibrary } from "../../controls/utils/icon-library";
 
 
 export class MakeArrayNodeParser extends NodeParser {
@@ -12,6 +13,6 @@ export class MakeArrayNodeParser extends NodeParser {
         data.node.title = 'Make Array';
         data.node.backgroundColor = Constants.DEFAULT_FUNC_PURE_BACKGROUND_COLOR;
 
-        return new HeadedNodeControl(data.node);
+        return new HeadedNodeControl(data.node, IconLibrary.MAKE_ARRAY);
     }
 }
