27700fa1bbeccf41131683f27304962d0406f3a3
Martin Hodler <martin.hodler@gmail.com>
2021-09-11 01:04:30 +0200

    Add support for CreateObject node

--- src/parser/node-parsers/create-object-node.parser.ts
+++ src/parser/node-parsers/create-object-node.parser.ts
@@ -2,9 +2,9 @@ import { NodeControl } from "../../controls/nodes/node.control";
 import { IconLibrary } from "../../controls/utils/icon-library";
 import { NodeParser } from "../node.parser";
 import { ParsingNodeData } from "../parsing-node-data";
-import { FoldableHeadedNodeControl } from "../../controls/nodes/foldable-headed-node.control";
 import { prettifyText } from "../../utils/text-utils";
 import { BlueprintParserUtils } from "../blueprint-parser-utils";
+import { HeadedNodeControl } from "../../controls/nodes/headed-node-control";
 
 export class CreateObjectNodeParser extends NodeParser {
 
@@ -12,6 +12,6 @@ export class CreateObjectNodeParser extends NodeParser {
         const className = BlueprintParserUtils.getFirstClassNameFromPinProperties(data.node.customProperties) || '';
         data.node.title = `Create ${prettifyText(className)}`;
 
-        return new FoldableHeadedNodeControl(data.node, IconLibrary.CONSTRUCT);
+        return new HeadedNodeControl(data.node, IconLibrary.CONSTRUCT);
     }
 }
