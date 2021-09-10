2c3bca8e148d6686fb7e0f6823f944667f1ff673
Pascal Aregger <pascal.aregger97@gmail.com>
2021-09-10 11:03:51 +0200

    Add support for additional timer functions

--- src/parser/node-friendly-names.ts
+++ src/parser/node-friendly-names.ts
@@ -1,3 +1,13 @@
 export const NodeFriendlyNames: { [name: string]: string; } = {
     'K2_SetTimerDelegate': 'Set Timer by Event',
+    'K2_SetTimer': 'Set Timer by Function Name',
+    'K2_PauseTimer': 'Pause Timer by Function Name',
+    'K2_ClearAndInvalidateTimerHandle': 'Clear and Invalidate Timer by Handle',
+    'K2_ClearTimer': 'Clear Timer by Function Name',
+    'K2_TimerExists': 'Does Timer Exist by Function Name',
+    'K2_TimerExistsHandle': 'Does Timer Exist by Handle',
+    'K2_GetTimerElapsedTime': 'Get Timer Elapsed Time by Function Name',
+    'K2_GetTimerElapsedTimeHandle': 'Get Timer Elapsed Time by Handle',
+    'K2_InvalidateTimerHandle': 'Invalidate',
+    'K2_IsTimerActiveHandle': 'Is Timer Active by Handle',
 }
